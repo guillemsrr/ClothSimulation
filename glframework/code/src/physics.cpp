b65016b2c831e48f364cbb321cc4d5e21d52d773
William Saw <guillemserralorenz@gmail.com>
2018-03-14 11:01:33 +0100

    falten col·lisions, però està tot plantejat!

--- glframework/code/src/physics.cpp
+++ glframework/code/src/physics.cpp
@@ -8,7 +8,10 @@
 void PhysicsInit();
 void verletSolver(float dt);
 void updateForces();
-glm::vec3 springForce(glm::vec3 p1, glm::vec3 p2, glm::vec3 v1, glm::vec3 v2, glm::vec2 k);
+glm::vec3 springForce(glm::vec3 p1, glm::vec3 p2, glm::vec3 v1, glm::vec3 v2, glm::vec2 k, float particleLinkDistance);
+void allParticlePlaneCollisions();
+void particlePlaneCollision(glm::vec3 normal, float d, int i, int j);
+void checkParticlePlaneCollision(glm::vec3 normal, float d, int i, int j);
 void sphereCollisions();
 
 //Render prims:
@@ -32,7 +35,8 @@ glm::vec3 gravityAccel = { 0.0f,-9.81,0.0f };
 glm::vec2 k_stretch = { 1000.0f,50.0f };
 glm::vec2 k_shear = { 1000.0f,50.0f };
 glm::vec2 k_bend = { 1000.0f,50.0f };
-float particleLinkDistance = 0.5f;
+float stretchDistance = 0.5f;
+float shearDistance, bendDistance;
 bool useCollisions = true;
 extern bool renderSphere = true;
 extern bool renderCloth = true;
@@ -40,12 +44,17 @@ float elasticCoefficient = 0.5f;
 float frictionCoefficient = 0.1f;
 #pragma endregion
 
-//Other variables:
+//Time:
 float resetTime;
+float deltaTime;
 const float reducer = 10.0f;
 //Sphere:
 glm::vec3 spherePosition;
 float sphereRadius;
+//Planes:
+glm::vec3 XplaneNormal = { 1,0,0 };
+glm::vec3 YplaneNormal = { 0,1,0 };
+glm::vec3 ZplaneNormal = { 0,0,1 };
 
 //Cloth:
 //Position:
@@ -87,7 +96,7 @@ void GUI()
 			ImGui::InputFloat2("k_stretch", (float*)&k_stretch);
 			ImGui::InputFloat2("k_shear", (float*)&k_shear);
 			ImGui::InputFloat2("k_bend", (float*)&k_bend);
-			ImGui::DragFloat("Particle Link", &particleLinkDistance, 0.05f);
+			ImGui::DragFloat("Particle Link Distance", &stretchDistance, 0.05f);
 
 			ImGui::TreePop();
 		}
@@ -116,6 +125,8 @@ void GUI()
 void PhysicsInit() {
 
 	resetTime = 0.0f;
+	shearDistance = sqrt(2)*stretchDistance;
+	bendDistance = 2 * stretchDistance;
 
 	//Initialize Sphere at random position
 	if (renderSphere)
@@ -129,12 +140,12 @@ void PhysicsInit() {
 	auxPos.z = -4.5f;
 	for (int i=0; i<18 ;i++)
 	{
-		auxPos.z += particleLinkDistance;
+		auxPos.z += stretchDistance;
 		auxPos.x = -4.0f;
 		for (int j=0; j<14 ;j++)
 		{
 			//Position:
-			auxPos.x += particleLinkDistance;
+			auxPos.x += stretchDistance;
 			posCloth[i][j] = { auxPos.x , 9.5f , auxPos.z };
 			lastPosCloth[i][j] = { auxPos.x , 9.5f , auxPos.z };
 			//Velocity:
@@ -146,6 +157,7 @@ void PhysicsInit() {
 }
 
 void PhysicsUpdate(float dt) {
+	deltaTime = dt / reducer;
 	if (playSimulation)
 	{
 		if (resetTime >= totalResetTime)
@@ -162,15 +174,15 @@ void PhysicsUpdate(float dt) {
 				//Forces:
 				updateForces();
 				//Position and Velocity:
-				verletSolver(dt/ reducer);
+				verletSolver(deltaTime);
 
 				//Collisions:
 				if (useCollisions)
 				{
-					sphereCollisions();
+					allParticlePlaneCollisions();
+					//sphereCollisions();
 				}
 			}
-			
 			ClothMesh::updateClothMesh((float*)posCloth);
 		}
 	}
@@ -197,58 +209,58 @@ void updateForces()
 				if (i - 1 >= 0)
 				{
 					//stretch up
-					sumFCloth[i][j]+= springForce(posCloth[i][j], posCloth[i-1][j], velCloth[i][j], velCloth[i-1][j], k_stretch);
+					sumFCloth[i][j]+= springForce(posCloth[i][j], posCloth[i-1][j], velCloth[i][j], velCloth[i-1][j], k_stretch, stretchDistance);
 					if (i - 2 >= 0)
 					{
 						//bending up
-						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i - 2][j], velCloth[i][j], velCloth[i - 2][j], k_bend);
+						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i - 2][j], velCloth[i][j], velCloth[i - 2][j], k_bend, bendDistance);
 					}
 					if (j - 1 >= 0)
 					{
 						//shear left
-						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i - 1][j - 1], velCloth[i][j], velCloth[i - 1][j - 1], k_shear);
+						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i - 1][j - 1], velCloth[i][j], velCloth[i - 1][j - 1], k_shear, shearDistance);
 					}
 					if (j + 1 < 14)
 					{
 						//shear right
-						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i - 1][j + 1], velCloth[i][j], velCloth[i - 1][j + 1], k_shear);
+						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i - 1][j + 1], velCloth[i][j], velCloth[i - 1][j + 1], k_shear, shearDistance);
 					}
 				}
 				//vertical stretch down
-				if (i + 1 <18)
+				if (i + 1 < 18)
 				{
-					sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i + 1][j], velCloth[i][j], velCloth[i + 1][j], k_stretch);
-					if (i + 2 <18)//bending down
+					sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i + 1][j], velCloth[i][j], velCloth[i + 1][j], k_stretch, stretchDistance);
+					if (i + 2 < 18)//bending down
 					{
-						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i + 2][j], velCloth[i][j], velCloth[i + 2][j], k_bend);
+						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i + 2][j], velCloth[i][j], velCloth[i + 2][j], k_bend, bendDistance);
 					}
 					if (j - 1 >= 0)
 					{
 						//shear left
-						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i + 1][j - 1], velCloth[i][j], velCloth[i + 1][j - 1], k_shear);
+						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i + 1][j - 1], velCloth[i][j], velCloth[i + 1][j - 1], k_shear, shearDistance);
 					}
 					if (j + 1 < 14)
 					{
-						//shear left
-						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i + 1][j + 1], velCloth[i][j], velCloth[i + 1][j + 1], k_shear);
+						//shear right
+						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i + 1][j + 1], velCloth[i][j], velCloth[i + 1][j + 1], k_shear, shearDistance);
 					}
 				}
 				//horizontal stretch left
 				if (j - 1 >= 0)
 				{
-					sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i][j-1], velCloth[i][j], velCloth[i][j-1], k_stretch);
+					sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i][j - 1], velCloth[i][j], velCloth[i][j - 1], k_stretch, stretchDistance);
 					if (j - 2 >= 0)//bending left
 					{
-						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i][j-2], velCloth[i][j], velCloth[i][j-2], k_bend);
+						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i][j - 2], velCloth[i][j], velCloth[i][j - 2], k_bend, bendDistance);
 					}
 				}
 				//horizontal stretch right
 				if (j + 1 < 14)
 				{
-					sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i][j+1], velCloth[i][j], velCloth[i][j+1], k_stretch);
+					sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i][j + 1], velCloth[i][j], velCloth[i][j + 1], k_stretch, stretchDistance);
 					if (j + 2 < 14)//bending right
 					{
-						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i][j+2], velCloth[i][j], velCloth[i][j+2], k_bend);
+						sumFCloth[i][j] += springForce(posCloth[i][j], posCloth[i][j + 2], velCloth[i][j], velCloth[i][j + 2], k_bend, bendDistance);
 					}
 				}
 			}
@@ -256,20 +268,17 @@ void updateForces()
 	}
 }
 
-glm::vec3 springForce(glm::vec3 p1, glm::vec3 p2, glm::vec3 v1, glm::vec3 v2, glm::vec2 k)
+glm::vec3 springForce(glm::vec3 p1, glm::vec3 p2, glm::vec3 v1, glm::vec3 v2, glm::vec2 k, float particleLinkDistance)
 {
-	//with damping
 	return -(k.x*(glm::distance(p1, p2) - particleLinkDistance) + k.y*glm::dot((v1 - v2), (p1 - p2) / glm::distance(p1, p2)))*(p1 - p2) / glm::distance(p1, p2);
-	//without damping
-	//return -(k.x*(glm::distance(p1, p2) - particleLinkDistance))*(p1 - p2) / glm::distance(p1, p2);
 }
 
 
 void verletSolver(float dt)
 {
-	for (int i = 0; i<18; i++)
+	for (int i = 0; i < 18; i++)
 	{
-		for (int j = 0; j<14; j++)
+		for (int j = 0; j < 14; j++)
 		{
 			if (i == 0 && (j == 0 || j == 13))
 			{
@@ -287,6 +296,48 @@ void verletSolver(float dt)
 			}
 		}
 	}
+}
+
+void allParticlePlaneCollisions()
+{
+	for (int i = 0; i < 18; i++)
+	{
+		for (int j = 0; j < 14; j++)
+		{
+			//left plane
+			checkParticlePlaneCollision(XplaneNormal, 5, i, j);
+			//right plane
+			checkParticlePlaneCollision(-XplaneNormal, 5, i, j);
+			//back plane
+			checkParticlePlaneCollision(ZplaneNormal, 5, i, j);
+			//front plane
+			checkParticlePlaneCollision(-ZplaneNormal, 5, i, j);
+			//down plane
+			checkParticlePlaneCollision(YplaneNormal, 0, i, j);
+			//up plane
+			checkParticlePlaneCollision(-YplaneNormal, 10, i, j);
+		}
+	}
+}
+
+void checkParticlePlaneCollision(glm::vec3 normal, float d, int i, int j)
+{
+	if ((glm::dot(normal, lastPosCloth[i][j]) + d)*(glm::dot(normal, posCloth[i][j]) + d) <= 0.f)
+	{
+		particlePlaneCollision(normal, d, i, j);
+	}
+}
+
+void particlePlaneCollision(glm::vec3 normal, float d, int i, int j)
+{
+
+	////Position:
+	//auxPos = posCloth[i][j];
+	//posCloth[i][j] = posCloth[i][j] + (posCloth[i][j] - lastPosCloth[i][j]) + (sumFCloth[i][j] / mass)*pow(deltaTime, 2);
+	//lastPosCloth[i][j] = auxPos;
+	////Velocity:
+	//lastVelCloth[i][j] = velCloth[i][j];
+	//velCloth[i][j] = (posCloth[i][j] - lastPosCloth[i][j]) / deltaTime;
 }
 
 void sphereCollisions()
